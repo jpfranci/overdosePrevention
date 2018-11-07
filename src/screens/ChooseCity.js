a899a57950b3c7aa928d03ec9728fd8e819baf36
Samson Keung <samsonkeung@hotmail.com>
2018-11-06 18:50:15 -0800

    handle Location Permission logic

--- src/screens/ChooseCity.js
+++ src/screens/ChooseCity.js
@@ -1,16 +1,24 @@
-import {Button, Picker, Text, View} from "react-native";
+import {Button, View, StyleSheet, Platform} from "react-native";
 import React from "react";
 import MapScreen from "./Map";
 import PropTypes from "prop-types";
 import OpsLogo from "../components/OpsLogo";
 import { Permissions } from 'expo';
+import RNPickerSelect from "react-native-picker-select";
+
+const PICKER_VALUES = {
+    ANONYMOUS: "anonymous",
+    USE_LOCATION: "use_location"
+};
 
 export default class ChooseCityScreen extends React.Component {
     static ScreenName = "ChooseCityScreen";
+    pickerValue = PICKER_VALUES.ANONYMOUS;
 
     constructor(props) {
         super(props);
-        this.state = {useLocation: false, locationPermissionGranted: false};
+        this.pickerValue = PICKER_VALUES.ANONYMOUS;
+        this.state = {useLocation: false};
     }
 
     componentDidUpdate() {
@@ -19,37 +27,29 @@ export default class ChooseCityScreen extends React.Component {
         if (this.state.useLocation) this._getLocationAsync();
     }
 
-    _getLocationAsync = async () => {
-        let { status } = await Permissions.askAsync(Permissions.LOCATION);
-        if (status !== 'granted') {
-            this.setState({
-                useLocation: false,
-            });
-        }
-
-        // let location = await Location.getCurrentPositionAsync({});
-        // this.setState({ location });
-    };
-
     render() {
         return (
             <View style={{ flex: 1,
                 backgroundColor: 'white',
-                alignItems: "center"
+                alignItems: "center",
+                flexDirection: "column"
             }}>
-                <View style={{ flex: 1, margin: 100}}>
-                    <OpsLogo height="100" width="100"/>
+                <View style={{ flex: 2, justifyContent: "center"}}>
+                    <OpsLogo height="110" width="110"/>
                 </View>
 
-                <View style={{flex: 3, margin: 40, alignItems: "center" }}>
-                    <Picker
-                        mode="dropdown"
-                        selectedValue={this.state.useLocation}
-                        style={{ height: 50, width: 250 }}
-                        onValueChange={(itemValue) => this.setState({useLocation: itemValue})}>
-                        <Picker.Item label="Anonymous" value={false} />
-                        <Picker.Item label="Enable location services" value={true} />
-                    </Picker>
+                <View style={{flex: 3, marginLeft: 40, marginRight: 40, alignItems: "center", justifyContent: "space-between", alignSelf: "stretch" }}>
+                    <RNPickerSelect
+                        placeholder={{}}
+                        value={this.pickerValue}
+                        style={{...pickerSelectStyles }}
+                        items={[
+                            {label: "Anonymous", value: PICKER_VALUES.ANONYMOUS},
+                            {label: "Enable location service", value: PICKER_VALUES.USE_LOCATION}
+                        ]}
+                        onValueChange={(value) => { this._onPickerValueChange(value); }}
+                        modalProps={{onDismiss: () => { this._updateLocationServiceState() }}}
+                    />
                     <Button
                         title=" Go To Map Screen"
                         onPress={() => this.props.navigation.navigate(MapScreen.ScreenName)}
@@ -60,8 +60,54 @@ export default class ChooseCityScreen extends React.Component {
             </View>
         );
     }
+
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.pickerValue = PICKER_VALUES.ANONYMOUS;
+            this.setState({useLocation: false});
+        }
+
+        // let location = await Location.getCurrentPositionAsync({});
+        // this.setState({ location });
+    };
+
+    _updateLocationServiceState() {
+        switch (this.pickerValue) {
+            case PICKER_VALUES.ANONYMOUS:
+                this.setState({useLocation: false});
+                break;
+            case PICKER_VALUES.USE_LOCATION:
+                this.setState({useLocation: true});
+                break;
+            default:
+                this.setState({useLocation: false});
+                break;
+        }
+    }
+
+    _onPickerValueChange(pickerVal) {
+        this.pickerValue = pickerVal;
+
+        if (Platform.OS === "android") {
+            this._updateLocationServiceState();
+        }
+    }
 }
 
 ChooseCityScreen.propTypes = {
     navigation: PropTypes.object.isRequired
 };
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 16,
+        paddingTop: 13,
+        paddingHorizontal: 10,
+        paddingBottom: 12,
+        borderBottomWidth: 1,
+        borderBottomColor: 'gray',
+        backgroundColor: 'white',
+        color: 'black',
+    },
+});
